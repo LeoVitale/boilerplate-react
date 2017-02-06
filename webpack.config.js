8f59c5bb6148dea61622a44e5edd29081e48ab54
Leonardo Vitale <leonardo_vitale@hotmail.com>
2017-02-06 16:09:14 -0200

    Inital Configs for Project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: '/dist/',
+    filename: 'bundle.js',
+
+  },
+  module: {
+    loaders: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig,
+            new OpenBrowserPlugin({ url: 'http://localhost:8080' })]
+}
